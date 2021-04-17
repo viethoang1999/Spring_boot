4340408524e46ba9642d6af5dc25ffd2cd481558
= <=>
2021-04-18 06:13:25 +0700

    Day file tao project

---
+++ Framwork/src/main/java/com/example/Framwork/FramworkApplication.java
@@ -0,0 +1,13 @@
+package com.example.Framwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FramworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FramworkApplication.class, args);
+	}
+
+}
---
+++ Framwork/src/test/java/com/example/Framwork/FramworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Framwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FramworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
